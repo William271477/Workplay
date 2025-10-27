40e82b7d0880a880d0a0fed09ac1a4ab927ae51f
William Ndhlovu <148003295+William271477@users.noreply.github.com>
2025-10-27 11:56:29 +0000

    Refactor main.jsx to simplify rendering by removing ErrorBoundary and AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App-simple";
-import ErrorBoundary from "./ErrorBoundary";
-import { AuthProvider } from "./contexts/AuthContext";
+import App from "./App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <ErrorBoundary>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </ErrorBoundary>
-);
+root.render(<App />);
